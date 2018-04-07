428142f2f96af8f908dc1f8ae85f54b716d8de79
zduch4c <zduch4c@airmail.cc>
2018-04-07 13:16:46 +0200

    overhaul

--- viskbd.el
+++ viskbd.el
@@ -1,77 +1,59 @@
-;; -*- lexical-binding: t -*-
-
 (defgroup viskbd nil
   "Display a visual on-screen keyboard."
   :prefix "viskbd-"
   :group 'convenience)
 
-(defcustom viskbd-layout-regular '((?` ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9 ?0 ?- ?= 0 ?\b)
-				   (?q ?w ?e ?r ?t ?y ?u ?i ?o ?p ?[ ?] 0 0 ?\r)
-				   (?a ?s ?d ?f ?g ?h ?j ?k ?l ?\; ?' ?\\ 0 0 ?\t)
-				   (?z ?x ?c ?v ?b ?n ?m ?, ?. ?/ 1 0)
-				   (?\s))
-  "The regular layout."
-  :group 'viskbd)
+;; 0 is empty space
+;; 1 is backspace
+;; 2 is return
+;; 3 is tab
+(setq viskbd-keymap [[?Q ?W ?E ?R ?T ?Y ?U ?I ?O ?P 0 1]
+		     [?A ?S ?D ?F ?G ?H ?J ?K ?L 0 0 2]
+		     [?Z ?X ?C ?V ?B ?N ?M 0 0 0 0 3]])
 
-(defcustom viskbd-layout-shifted '((?~ ?! ?@ ?# ?$ ?% ?^ ?& ?* ?( ?) ?_ ?+ 0 ?\b)
-				   (?Q ?W ?E ?R ?T ?Y ?U ?I ?O ?P ?{ ?} 0 0 ?\r)
-				   (?A ?S ?D ?F ?G ?H ?J ?K ?L ?: ?\" ?| 0 0 ?\t)
-				   (?Z ?X ?C ?V ?B ?N ?M ?< ?> ?? 1 0)
-				   (?\s))
-  "The layout when shift is pressed."
-  :group 'viskbd)
-
-(defcustom viskbd-keyboard-map
+(defvar viskbd-button-map
   (let ((map (make-sparse-keymap)))
-    (define-key map [(control ?m)] 'push-button)
     (define-key map [mouse-1] 'push-button)
     map)
-  "Keymap used by buttons."
-  :group 'viskbd)
+  "The button map of the visual keyboard keys.")
 
-(defvar shifted-flag nil
-  "Is the shift key pressed?")
+(define-button-type 'viskbd-base
+  'help-echo (purecopy "mouse-1, RET: Push this button")
+  'keymap viskbd-button-map
+  'face 'tool-bar
+  'mouse-face 'tool-bar)
 
-(define-button-type 'viskbd-shift
-  'keymap viskbd-keyboard-map
-  'face 'custom-button-mouse
-  'action (lambda (button)
-	    (if (not shifted-flag) (progn (setf shifted-flag t) (viskbd-display-kbd layout-shifted))
-	      (progn (setf shifted-flag nil) (viskbd-display-kbd layout-regular)))))
-(define-button-type 'viskbd-return
-  'keymap viskbd-keyboard-map
-  'face 'custom-button-mouse
-  'action (lambda (button) (other-window 1) (newline) (other-window -1)))
+(define-button-type 'viskbd-key
+  :supertype 'viskbd-base
+  'action (lambda (button) (message "key pressed")))
+
 (define-button-type 'viskbd-backspace
-  'keymap viskbd-keyboard-map
-  'face 'custom-button-mouse
-  'action (lambda (button) (other-window 1) (backward-delete-char-untabify 1) (other-window -1)))
-(define-button-type 'viskbd-tab
-  'keymap viskbd-keyboard-map
-  'face 'custom-button-mouse
-  'action (lambda (button) (other-window 1) (indent-for-tab-command) (other-window -1)))
+  :supertype 'viskbd-base
+  'action (lambda (button) (message "backspace pressed")))
 
-(defun viskbd-display-kbd (layout)
-  (when buffer-read-only (setf buffer-read-only nil))
-  (erase-buffer)
-  (dolist (row layout)
-    (dolist (key row)
-      (define-button-type 'temporary-normal-key
-	'keymap viskbd-keyboard-map
-	'face 'custom-button-mouse
-      	'action (lambda (button) (other-window 1) (insert key) (other-window -1)))
-      (cond ((= key 0) (insert "   "))
-	    ((= key 1) (insert-button "  SHIFT  " :type 'viskbd-shift))
-	    ((= key ?\r) (insert-button " RET " :type 'viskbd-return))
-	    ((= key ?\s) (insert-button "                   SPC                 " :type 'temporary-normal-key))
-	    ((= key ?\b) (insert-button " BSP " :type 'viskbd-backspace))
-	    ((= key ?\t) (insert-button " TAB " :type 'viskbd-tab))
-	    (t (insert-button (format " %c " key) :type 'temporary-normal-key :face 'custom-button))))
-    (insert "\n"))
-  (when (not buffer-read-only) (setf buffer-read-only t)))
+(define-button-type 'viskbd-return
+  :supertype 'viskbd-base
+  'action (lambda (button) (message "return pressed")))
+
+(define-button-type 'viskbd-tab
+  :supertype 'viskbd-base
+  'action (lambda (button) (message "tab pressed")))
+
+(defun viskbd-handle-key (key)
+  (cond ((= key 0) (insert " "))
+	((= key 1) (insert-button "BSP" :type 'viskbd-backspace))
+	((= key 2) (insert-button "RET" :type 'viskbd-return))
+	((= key 3) (insert-button "TAB" :type 'viskbd-tab))
+	(t (insert-button key :type 'viskbd-key))))
+
+(defun viskbd-display (keymap)
+  "Display the keymap."
+  (mapcar (lambda (row) (mapcar 'viskbd-handle-key row) (insert "\n")) keymap))
 
 (defun viskbd ()
+  "Display a visual on-screen keyboard."
   (interactive)
   (let ((buffer (get-buffer-create "*viskbd*")))
     (pop-to-buffer buffer)
-    (viskbd-display-kbd layout-regular)))
+    (viskbd-display viskbd-keymap)
+    (setq buffer-read-only t)))
